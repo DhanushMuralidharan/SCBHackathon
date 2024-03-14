03143f7e4a4ffacac8e876dc3dc61985749404e2
DhanushMuralidharan <dhanushmuralidharan@gmail.com>
2024-03-14 15:42:45 +0530

    Link some pages

--- controllers.py
+++ controllers.py
@@ -1,6 +1,19 @@
 from config import app
 from models import *
+from flask import render_template
 
 @app.route('/')
 def index():
     return "Welcome to SCB Hackathon Home Page"
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/incoming')
+def incoming():
+    return render_template('incoming_cheque.html')
+
+@app.route('/outgoing')
+def outgoing():
+    return render_template('outgoing_cheque.html')
